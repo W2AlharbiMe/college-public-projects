20e0a13b918665ec90ec9ae760415d642cfca84a
Abdullah Alharbi <43024659+W2AlharbiMe@users.noreply.github.com>
2021-09-19 20:44:30 +0300

    Create app2.py

---
+++ selected-subject/exercises/app2.py
@@ -0,0 +1,46 @@
+## 2021-09-19 20:43:55
+
+import pandas as pd
+
+## Read csv
+csv1 = pd.read_csv('./dataFile.csv')
+
+## create dataframe
+df1 = pd.DataFrame(csv1)
+
+## [x] Print first 10 rows
+print("first 10 rows")
+print(df1.head(10))
+
+print('-------------------------------------------')
+## [x] print last 5 rows
+print("last 5 rows")
+print(df1.tail(5))
+
+print('-------------------------------------------')
+
+## [x] Create new dataframe by removing rows with empty cells (how many?)
+df2 = pd.DataFrame(df1)
+
+print("Create new dataframe by removing rows with empty cells (how many?)")
+print(df2.dropna().count())
+
+print('-------------------------------------------')
+
+## [ ] create new dataframe by replacing empty values with '111'
+df3 = pd.DataFrame(df1)
+
+
+## [x] print mean, median for Pulse column
+
+print("mean, median - Pulse column")
+print(df1['Pulse'].mean())
+print(df1['Pulse'].median())
+
+
+### [x] sort dataframe by column
+
+df1last5sorted = df1.tail(5).sort_values(by='Duration', ascending=False)
+print('-------------------------------------------')
+
+print(df1last5sorted)
