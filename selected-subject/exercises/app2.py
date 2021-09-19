55e258aa2b41b7b82be203380420bc809ca9cf4f
Abdullah Alharbi <43024659+W2AlharbiMe@users.noreply.github.com>
2021-09-19 21:12:33 +0300

    Update app2.py

--- selected-subject/exercises/app2.py
+++ selected-subject/exercises/app2.py
@@ -1,9 +1,10 @@
 ## 2021-09-19 20:43:55
 
 import pandas as pd
+from pandas._libs.missing import NA
 
 ## Read csv
-csv1 = pd.read_csv('./dataFile.csv')
+csv1 = pd.read_csv('../../dataFile.csv')
 
 ## create dataframe
 df1 = pd.DataFrame(csv1)
@@ -30,6 +31,10 @@ print('-------------------------------------------')
 ## [ ] create new dataframe by replacing empty values with '111'
 df3 = pd.DataFrame(df1)
 
+print('-------------------------------------------')
+
+print(df3.fillna('111').loc[0:20])
+print('-------------------------------------------')
 
 ## [x] print mean, median for Pulse column
 
