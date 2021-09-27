ce599a5f6b47f31e774fcba03c242d083320ab7c
Abdullah Alharbi <43024659+W2AlharbiMe@users.noreply.github.com>
2021-09-27 16:48:28 +0300

    Create app6.py

---
+++ python-101/labs/app6.py
@@ -0,0 +1,25 @@
+# Bug hunter
+
+terminate = False
+days_count = 1
+total_bugs = 0
+
+while not terminate:
+    bugs = int(input("Day ({}) Enter the number of the bugs(enter 0 to exit): ".format(days_count)))
+
+    # terminate program if user enter 0
+    if bugs == 0:
+        terminate = True
+
+    # Validation Rule
+
+    if bugs < 0:
+        print("the bugs number should be greater than 0.")
+        continue
+
+
+    total_bugs += bugs
+    days_count += 1
+
+print("Total Bugs is {} in {} Days".format(total_bugs, days_count))
+
